662fc05817fa482d15628f6f66a18dfc4f4dbc38
Carlos Castilla <c.castilla@uniandes.edu.co>
2021-07-20 19:32:18 -0500

    Probando Consumir datos en Gist
    Con el objetivo de no tener que pedir directamente a la API datos para desarrollo y no interferir con la página, se volcaron los datos de 120 profesores a un gist de github, desde el cual se consumen los datos en formato json. Este commite tiene como objetivo mostrar cómo consumir dichos datos para renderizarlos.

--- redes-conocimiento/src/App.js
+++ redes-conocimiento/src/App.js
@@ -1,10 +1,31 @@
 
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [profesores, setProfesores] = useState([]);
+
+  useEffect(() => {
+    fetch("https://gist.githubusercontent.com/ccastillao/55276c22850c7a52da6d2710c5178677/raw/profesores.json")
+      .then(res => {
+        res.json().then(resj => { setProfesores(resj); console.log(resj) })
+      });
+
+  }, [])
   return (
     <div className="App">
-      <h1>Base - Proyecto Redes de Conocimiento</h1>
+      {profesores
+        ?
+        <div>
+          {
+            profesores.map((p, index) => (<div key={p.div}><img src={p.otros_campos.fotografia.sizes.thumbnail} alt="" /><h6>{p.title.rendered}</h6> <p>{index}</p> </div>)
+
+            )
+          }
+        </div>
+
+        :
+        "loading..."}
     </div>
   );
 }
